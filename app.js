874de3c17c6ccb985988d3ff580c38e6b19f3647
megha-wadhwa12 <megha.wadhwa@kalvium.community>
2023-10-26 02:34:47 +0530

    numeron2

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 // Iteration 1: Making the play button in the index.html functional.
 // Description: When the play button is clicked the game.html page should be opened
+
+var playbtn = document.getElementById("play-button")
+playbtn.onclick=()=>{
+    window.location.href = "./game.html"
+    // window.open("./game.html",)
+}
+
+
--- score.js
+++ score.js
@@ -1,1 +1,16 @@
 // Iteration 8: Making scoreboard functional
+
+let scoreBoard = document.getElementById("score-board")
+console.log(window.location.search);
+
+let params = new URLSearchParams(window.location.search)
+
+let score = params.get("score")
+console.log("score: ",score);
+
+scoreBoard.textContent = score;
+
+let playagainbtn = document.getElementById("play-again-button")
+playagainbtn.onclick = ()=>{
+    window.location.href = "./game.html"
+}
